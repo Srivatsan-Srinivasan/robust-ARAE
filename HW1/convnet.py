9e5b3ac500646c25f914f5b5c6a0a2e52c891ffd
Sebastien Baur <sebastien.baur@hotmail.fr>
2018-01-29 17:59:00 -0500

    new CNN architecture

--- HW1/convnet.py
+++ HW1/convnet.py
@@ -17,73 +17,20 @@ def vectorize(text, TEXT, vdim=300):
 
 
 class Convnet(t.nn.Module):
-    """Note that optimizer is hardcoded to Adam"""
-
-    def __init__(self, learning_rate):
+    def __init__(self):
         super(Convnet, self).__init__()
-        self.conv1 = conv(300, 100, 5, padding=2)
-        #         self.conv2 = conv(100, 100, 3, padding=1)
-        self.fc1 = fc(100, 100)
-        self.fc2 = fc(100, 2)
-        self.optimizer = t.optim.Adam(self.parameters(), lr=learning_rate)
-        self.loss = t.nn.NLLLoss(size_average=True)
+        self.conv1 = conv(300, 50, 3, padding=1)
+        # self.conv2 = conv(50, 50, 3, padding=1)
+        self.fc2 = fc(50, 2)
 
     def forward(self, x):
-        dropout = Dropout(.2)
-        xx = self.conv1(x)
-        #         xx = tanh(xx)
-        #         xx = self.conv2(xx)
-        xx = tanh(t.max(xx, -1)[0])
-        xx = self.fc1(xx)
-        #         xx = dropout(xx)
-        xx = tanh(xx)
-        xx = self.fc2(xx)
+        dropout = Dropout(.25)
+        xx = relu(self.conv1(x))
+        xx = self.conv2(xx)
+        xx = relu(t.max(xx, -1)[0])
         xx = dropout(xx)
+        xx = self.fc2(xx)
         return softmax(xx)
-
-    def eval_perf(self, iterator, TEXT, vdim=300):
-        count = 0
-        bs = iterator.batch_size * 1
-        iterator.batch_size = 1
-        for i, batch in enumerate(iterator):
-            # get data
-            text = batch.text
-            y_pred = (self.forward(variable(vectorize(text, TEXT, vdim=vdim)))[:, 1] > 0.5).long()
-            y = batch.label.long() * (-1) + 2
-
-            count += t.sum((y == y_pred).long())
-            if i >= len(iterator) - 1:
-                break
-        iterator.batch_size = bs
-        return (count.float() / (bs*len(iterator))).data.numpy()[0]
-
-    def fit(self, train_iter, val_iter, n_epochs, TEXT, vdim=300):
-
-        for _ in range(n_epochs):
-            for i, batch in enumerate(train_iter):
-                # get data
-                text = batch.text
-                y_pred = self.forward(variable(vectorize(text, TEXT, vdim=vdim)))
-                y = batch.label.long() * (-1) + 2
-
-                # initialize gradients
-                self.optimizer.zero_grad()
-
-                # loss
-                loss = self.loss.forward(y_pred, y)
-
-                # compute gradients
-                loss.backward()
-
-                # update weights
-                self.optimizer.step()
-
-                if i >= len(train_iter) - 1:
-                    break
-            train_iter.init_epoch()
-            self.eval()
-            print("Validation accuracy after %d epochs: %.2f" % (_, self.eval_perf(val_iter, TEXT, vdim=vdim)))
-            self.train()
 
 
 def main(n_epochs, learning_rate, vdim=300):
