4168f29fd66f2511b4eea91fc1d106b11ed80489
Sebastien Baur <sebastien.baur@hotmail.fr>
2018-02-10 18:06:23 -0500

    debug nnlm

--- HW2/language_models.py
+++ HW2/language_models.py
@@ -91,6 +91,7 @@ class NNLM(t.nn.Module):
     def __init__(self, params, embeddings):
         super(NNLM, self).__init__()
         self.model_str = 'NNLM'
+        print(params)
         self.context_size = int(params.get('con_size'))
         self.train_embedding = params.get('train_embedding', False)
         self.vocab_size = embeddings.size(0)
