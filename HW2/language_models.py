trying to fix the memory overflow pb

HW2/language_models.py
@@ -132,15 +132,16 @@ class NNLM2(t.nn.Module):
         self.w.weight = t.nn.Parameter(embeddings, requires_grad=self.train_embedding)
 
         # self.dropout = t.nn.Dropout()
-        self.fc1 = t.nn.Linear(self.embed_dim*self.context_size, 100)
-        self.fc11 = t.nn.Linear(100, self.vocab_size, bias=False)
+        self.fc1 = t.nn.Linear(self.embed_dim*self.context_size, 50)
+        self.fc11 = t.nn.Linear(50, self.vocab_size, bias=False)
         self.fc2 = t.nn.Linear(self.embed_dim*self.context_size, self.vocab_size)
 
     def forward(self, x):
         xx = self.w(x).transpose(2, 1)
         # xx = self.dropout(xx)
-        xx1 = self.fc11(F.tanh(self.fc1(xx.contiguous().view(xx.size(0), -1))))
-        xx2 = self.fc2(xx.contiguous().view(xx.size(0), -1))
+        xx = xx.contiguous().view(xx.size(0), -1)  # .contiguous() because .view() requires the tensor to be stored in contiguous memory blocks
+        xx1 = self.fc11(F.tanh(self.fc1(xx)))
+        xx2 = self.fc2(xx)
         xx = xx1 + xx2
         return xx
 