1a08443ab276a05fb55d23e540b8be88ed57d6a3
Sebastien Baur <sebastien.baur@hotmail.fr>
2018-02-25 20:00:49 -0500

    fix dim init_hidden for LSTMA in case of deep models

--- HW3/translation_models.py
+++ HW3/translation_models.py
@@ -364,7 +364,7 @@ class LSTMA(t.nn.Module):
         # 2 tensors having as first dim twice the hidden dim you set
         self.encoder_rnn = t.nn.LSTM(self.embedding_dim, self.hidden_dim // 2, dropout=self.dropout, num_layers=self.num_layers, bidirectional=True, batch_first=True)
         self.decoder_rnn = t.nn.LSTM(self.embedding_dim, self.hidden_dim, dropout=self.dropout, num_layers=self.num_layers, batch_first=True)
-        self.hidden_dec_initializer = t.nn.Linear(self.hidden_dim // 2, self.hidden_dim)
+        self.hidden_dec_initializer = t.nn.Linear(self.hidden_dim // 2, self.num_layers * self.hidden_dim)
         self.hidden2out = t.nn.Linear(self.hidden_dim * 2, self.output_size)
         if self.embed_dropout:
             self.dropout_1s = t.nn.Dropout(self.dropout)
@@ -398,8 +398,9 @@ class LSTMA(t.nn.Module):
             # `:` for the whole batch
             # `:1` because you want the hidden state of the first time step (see paper, they use backward(h1))
             # but also `self.hidden_dim // 2:`, because you want the backward part only (the last coefficients)
-            h = F.tanh(self.hidden_dec_initializer(data[:, :1, self.hidden_dim // 2:]))  # @todo: verify that the last hdim/2 weights actually correspond to the backward layer(s)
+            h = F.tanh(self.hidden_dec_initializer(data[:, :1, self.hidden_dim // 2:]))  # the last hdim/2 weights correspond to the backward layer(s)
             h = h.transpose(1, 0)
+            h = t.cat(t.split(h, self.hidden_dim, dim=2), 0)
             return (
                 h,
                 variable(np.zeros((self.num_layers, bs, self.hidden_dim)), cuda=self.cuda_flag)
