30fa84a0eef9eb47357aa35287a573d345a79bff
Sebastien Baur <sebastien.baur@hotmail.fr>
2018-02-28 13:16:43 -0500

    fix target embedding in lstmr

--- HW3/translation_models.py
+++ HW3/translation_models.py
@@ -235,7 +235,7 @@ class LSTMR(t.nn.Module):
         else:
             xx_source = x_source
         embedded_x_source = self.source_embeddings(xx_source)
-        embedded_x_target = self.target(x_target[:, :-1])  # don't take into account the last token because there is nothing after
+        embedded_x_target = self.target_embeddings(x_target[:, :-1])  # don't take into account the last token because there is nothing after
         if self.embed_dropout:
             embedded_x_source = self.dropout_1s(embedded_x_source)
             embedded_x_target = self.dropout_1t(embedded_x_target)
