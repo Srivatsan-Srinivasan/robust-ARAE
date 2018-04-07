3365affc269881150c62f668c8b7a25b55a90922
Ubuntu <sbssbh@cs287.t4bxcvg4gfguhcetjpifa5l5fg.xx.internal.cloudapp.net>
2018-04-07 05:47:50 +0000

    Add bleu test

--- bleu_test/bleu_test.py
+++ bleu_test/bleu_test.py
@@ -1,6 +1,7 @@
-reference = 'Two person be in a small race car drive by a green hill .'
-output = 'Two person in race uniform in a street car .'
+reference = 'Two person be in a small race car drive by a green hill . It is a beautiful morning .'
+output = 'Two person in race uniform in a street car .It is a beautiful morning .'
 
+#output = reference
 with open('output', 'w') as output_file:
     output_file.write(reference)
 
