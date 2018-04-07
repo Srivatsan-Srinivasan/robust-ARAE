ff1f4b4e1ef6f5cdba55e1aad08a2081d83ba040
Ubuntu <sbssbh@cs287.t4bxcvg4gfguhcetjpifa5l5fg.xx.internal.cloudapp.net>
2018-04-07 06:05:03 +0000

    Save bleu as txt file

--- bleu_test/bleu_test.py
+++ bleu_test/bleu_test.py
@@ -9,4 +9,14 @@ with open('reference', 'w') as reference_file:
     reference_file.write(output)
 
 from os import system
-system('./multi-bleu.perl reference < output')
+cmd = './multi-bleu.perl reference < output'
+system(cmd)
+#import subprocess
+#out = subprocess.check_output(cmd)
+#print(output)
+import os
+bleu_output = os.popen(cmd).readlines()
+with open('bleu_scores.txt', 'w') as fout:
+    for i in bleu_output:
+    	fout.write(i)
+
