855c958415133ba1f22491084fea8a1c17a7909a
Sebastien Baur <sebastien.baur@hotmail.fr>
2018-04-09 16:58:56 -0400

    fix tensorboard issue (dataparallel vs non parallel)

--- pytorch/train.py
+++ pytorch/train.py
@@ -589,7 +589,7 @@ for epoch in range(1, args.epochs+1):
 
         niter_global += 1
 
-        tensorboard(niter_global, writer, gan_gen, gan_disc, autoencoder, args.tensorboard_freq)  # @todo: solve this - as it is now, the autoencoder has its gradients erased just before the call of the function
+        tensorboard(niter_global, writer, gan_gen, gan_disc, autoencoder, args.tensorboard_freq) if args.n_gpus == 1 else tensorboard(niter_global, writer, gan_gen.module, gan_disc.module, autoencoder.module, args.tensorboard_freq) # @todo: solve this - as it is now, the autoencoder has its gradients erased just before the call of the function
         if niter_global % 100 == 0:
             print('[%d/%d][%d/%d] Loss_D: %.8f (Loss_D_real: %.8f '
                   'Loss_D_fake: %.8f) Loss_G: %.8f'
