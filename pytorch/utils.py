b90352b8e5e6e1de9be0a647385952c93050e4af
Sebastien Baur <sebastien.baur@hotmail.fr>
2018-04-12 17:13:55 -0400

    debug select_gpu

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -23,14 +23,9 @@ def check_args(args):
 def select_gpu(gpu_id):
     if gpu_id is not None:
         os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
-        # to prevent opencv from initializing CUDA in workers
-        t.randn(8).cuda()
-        os.environ['CUDA_VISIBLE_DEVICES'] = ''
 
 
 def create_tensorboard_dir(logdir):
-
-    #import pdb; pdb.set_trace()
     if logdir not in os.listdir('tensorboard/'):
         os.makedirs('tensorboard/'+logdir)
 
