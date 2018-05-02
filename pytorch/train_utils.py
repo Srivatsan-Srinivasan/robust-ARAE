6693b9fadbff9493b0d39c39c690d96d1accef4b
Sebastien Baur <sebastien.baur@hotmail.fr>
2018-05-02 16:54:40 -0400

    debug synthetic exp

--- pytorch/train_utils.py
+++ pytorch/train_utils.py
@@ -241,14 +241,15 @@ def train_lm_synthetic(gan_gen, autoencoder, oracle, args):
 
     # Evaluate the PPL with the oracle
     indices = np.concatenate(indices, axis=0).squeeze()
-    lengths = np.argmax(indices == 2,  1).tolist()  # 2 is the EOS token. This thing finds the first occurence of the EOS token, its idx is the length
-    indices_lengths = list(sorted([(i, l) for i, l in zip(indices, lengths)], reverse=True, key=lambda x: x[1]))
-    indices = []
-    lengths = []
-    for (i, l) in indices_lengths:
-        indices.append(i.reshape((1, -1)))
-        lengths.append(l)
-    indices = np.concatenate(indices, axis=0)
+    lengths = 500*[indices.shape[1]]
+    # lengths = np.argmax(indices == 2,  1).tolist()  # 2 is the EOS token. This thing finds the first occurence of the EOS token, its idx is the length
+    # indices_lengths = list(sorted([(i, l) for i, l in zip(indices, lengths)], reverse=True, key=lambda x: x[1]))
+    # indices = []
+    # lengths = []
+    # for (i, l) in indices_lengths:
+    #     indices.append(i.reshape((1, -1)))
+    #     lengths.append(l)
+    # indices = np.concatenate(indices, axis=0)
     indices = variable(indices, gpu_id=args.gpu_id, cuda=args.cuda, to_float=False).long()
     indices = (indices - 3)*((indices > 2).long()) + args.ntokens*((indices <= 2).long())
     ppl = oracle.get_ppl(indices, lengths)
